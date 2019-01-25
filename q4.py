018b2c423a5e2289259b93d5fc6c0abd385b9c7d
Anirudddha08 <47025750+Anirudddha08@users.noreply.github.com>
2019-01-25 14:45:34 +0530

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+total=a+b+c
+avg=(total/3)
+print("Total sum of three numbers is ",total)
+print("Average of three numbers is ",avg)
